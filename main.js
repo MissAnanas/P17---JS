e3f8f1351b513e07d0ab86a225fb6d76e383abb5
Anaïs 3 <pintoanais69@gmail.com>
2025-04-07 11:58:12 +0200

    bientot fini

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ function randomizeName() {
 }
 
 const assets = {
-    face: ["Face/face1.png", "Face/face2.png", "Face/face3.png"],  // Modification du chemin
-    hair: ["Hair/cheveux1.png", "Hair/cheveux2.png", "Hair/cheveux3.png"],  // Modification du chemin
-    shirt: ["Shirts/Blue/blueShirt1.png", "Shirts/Blue/blueShirt2.png", "Shirts/Blue/blueShirt3.png", "Shirts/Blue/blueShirt4.png", "Shirts/Blue/blueShirt5.png", "Shirts/Blue/blueShirt6.png", "Shirts/Blue/blueShirt7.png", "Shirts/Blue/blueShirt8.png",
-        "Shirts/green/greenShirt1.png", "Shirts/green/greenShirt2.png", "Shirts/green/greenShirt3.png", "Shirts/green/greenShirt4.png", "Shirts/green/greenShirt5.png", "Shirts/green/greenShirt6.png", "Shirts/green/greenShirt7.png", "Shirts/green/greenShirt8.png"],  // Modification du chemin
+    face: ["Face/face1.png", "Face/face2.png", "Face/face3.png", "Face/face4.png"],
+    hair: ["Hair/cheveux1.png", "Hair/cheveux2.png", "Hair/cheveux3.png", "Hair/cheveux4.png", "Hair/cheveux5.png", "Hair/cheveux6.png", "Hair/cheveux7.png", "Hair/cheveux8.png", 
+        "Hair/cheveux9.png", "Hair/cheveux10.png", "Hair/cheveux11.png"],
+    shirt: ["Shirts/Blue/blueShirt1.png", "Shirts/Blue/blueShirt2.png", "Shirts/Blue/blueShirt3.png", "Shirts/Blue/blueShirt4.png", "Shirts/Blue/blueShirt5.png", 
+        "Shirts/Blue/blueShirt6.png", "Shirts/Blue/blueShirt7.png", "Shirts/Blue/blueShirt8.png",
+        "Shirts/green/greenShirt1.png", "Shirts/green/greenShirt2.png", "Shirts/green/greenShirt3.png", "Shirts/green/greenShirt4.png", "Shirts/green/greenShirt5.png", 
+        "Shirts/green/greenShirt6.png", "Shirts/green/greenShirt7.png", "Shirts/green/greenShirt8.png"],
     skin: ["Skin/tint1_head.png", "Skin/tint2_head.png", "Skin/tint3_head.png", "Skin/tint3_head.png", "Skin/tint4_head.png", "Skin/tint5_head.png", "Skin/tint6_head.png",
-    "Skin/tint7_head.png", "Skin/tint8_head.png",]   // Modification du chemin
+    "Skin/tint7_head.png", "Skin/tint8_head.png",]
 };
 
 let currentCategory = 'face';
@@ -53,7 +56,6 @@ function updateCharacter(asset) {
             break;
     }
 }
-
 
 
 updateSelectionGrid();
